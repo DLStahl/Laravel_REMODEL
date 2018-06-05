f3bbfe343038c81849b53b093538feacf83d7aae
Xing Gao <youzi_g@163.com>
2018-06-05 07:13:50 -0400

    delete <p>{{5}}</p>

--- blog/resources/views/pages/about.blade.php
+++ blog/resources/views/pages/about.blade.php
@@ -6,7 +6,6 @@
     <div class="row">
         <div class="col-md-12">
             <h1>About {{ $data['fullname'] }}</h1>
-            <p>{{ 5 }}</p>
             <p>
                This is project capstone cse 5911
             </p>
