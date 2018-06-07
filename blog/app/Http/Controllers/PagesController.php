39e9a1d1fac77f7e9f3a4c1fc4f1fea2e1d605fd
costyneuf <youzi_g@163.com>
2018-06-06 22:33:11 -0400

    add PagesController

---
+++ blog/app/Http/Controllers/PagesController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Http\Controllers;
+class PagesController extends Controller
+{
+	public function getIndex()
+	{
+		#process variable data
+		#talk to the model
+		#receive data back from model
+		#compile or process data from the model if needed
+		#pass that data to the correct view
+		return view('pages.welcome');
+	}
+	public function getAbout()
+	{
+		$first = "Yi";
+		$last = "XU";
+		$full = $first . " " . $last;
+		$email = "xu.1882@osu.edu";
+		$data['fullname'] = $full;
+		$data['email'] = $email;
+		//return view('pages.about')->withFullname($full)->withEmail($email);
+		return view('pages.about')->withData($data);
+	}
+	public function getContact()
+	{
+		return view('pages.contact');
+	}
+	public function getPage(){
+		return view('pages.page');
+	}
+}
