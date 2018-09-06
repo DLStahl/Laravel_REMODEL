e8b6dfcf00e90d533997492b82a4ce4fb9ef7747
remodel.anesthesiology_dev <remodel.anesthesiology_dev@whadmprd01.it.ohio-state.edu>
2018-09-06 13:04:57 -0400

    Changed "personal information" to "My Selections" in the nav bar

--- blog/resources/views/partials/_nav.blade.php
+++ blog/resources/views/partials/_nav.blade.php
@@ -18,7 +18,7 @@
                 <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="about">About Me</a>
+                <a class="nav-link" href="about">My Selections</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="contact">Contact Me</a>
